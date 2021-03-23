81db5f98e576038e3ab6c8e5a6da4f8fea237cf7
Rakshith I <raksht7@gmail.com>
2021-03-24 02:01:18 +0530

    Updated Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 
 const Home = (props) => {
 
     return (
         <div style={{textAlign : "center"}}>
-            <h1>Home Component</h1>
+            
+            <Grid container spacing={2}>
+                <Grid item xs={6} style={{textAlign : 'left'}} >
+                    <h1>Hello User...</h1>
+                    <p >
+                        Velit do occaecat sint incididunt ex in consectetur aute sint sunt. Ex culpa officia incididunt aliquip nulla minim. Aliqua magna sit dolore eu veniam commodo tempor veniam eu irure deserunt dolor tempor amet.Velit do occaecat sint incididunt ex in consectetur aute sint sunt. Ex culpa officia incididunt aliquip nulla minim. Aliqua magna sit dolore eu veniam commodo tempor veniam eu irure deserunt dolor tempor amet.Velit do occaecat sint incididunt ex in consectetur aute sint sunt. Ex culpa officia incididunt aliquip nulla minim. Aliqua magna sit dolore eu veniam commodo tempor veniam eu irure deserunt dolor tempor amet.Velit do occaecat sint incididunt ex in consectetur aute sint sunt. Ex culpa officia incididunt aliquip nulla minim. Aliqua magna sit dolore eu veniam commodo tempor veniam eu irure deserunt dolor tempor amet.</p>
+                </Grid>
+
+                <Grid item xs={6}>
+                   <img src="https://via.placeholder.com/250" alt=""></img>
+                </Grid>
+            </Grid>
             <p>@Created by Rakshith</p>
         </div>
     )
