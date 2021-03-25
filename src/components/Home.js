086e20f73e91bbdf2f9b0d4949e766574265ca82
Rakshith I <raksht7@gmail.com>
2021-03-25 20:44:14 +0530

    Updated Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Grid, Typography } from '@material-ui/core'
+import EmojiPeopleSharpIcon from '@material-ui/icons/EmojiPeopleSharp';
 
 const Home = (props) => {
 
@@ -8,9 +9,18 @@ const Home = (props) => {
             
             <Grid container spacing={2}>
                 <Grid item xs={6} style={{textAlign : 'left'}} >
-                    <h1>Hello User...</h1>
-                    <p >
-                        Velit do occaecat sint incididunt ex in consectetur aute sint sunt. Ex culpa officia incididunt aliquip nulla minim. Aliqua magna sit dolore eu veniam commodo tempor veniam eu irure deserunt dolor tempor amet.Velit do occaecat sint incididunt ex in consectetur aute sint sunt. Ex culpa officia incididunt aliquip nulla minim. Aliqua magna sit dolore eu veniam commodo tempor veniam eu irure deserunt dolor tempor amet.Velit do occaecat sint incididunt ex in consectetur aute sint sunt. Ex culpa officia incididunt aliquip nulla minim. Aliqua magna sit dolore eu veniam commodo tempor veniam eu irure deserunt dolor tempor amet.Velit do occaecat sint incididunt ex in consectetur aute sint sunt. Ex culpa officia incididunt aliquip nulla minim. Aliqua magna sit dolore eu veniam commodo tempor veniam eu irure deserunt dolor tempor amet.</p>
+                    <h1>Hello User<EmojiPeopleSharpIcon />...</h1>
+                    <h2>If you are new to this App, follow the steps bellow: </h2>
+                    <br/>
+                    <Typography variant="p">
+                        <li>Register your account.</li>
+                        <li>Login with your credentials.</li>
+                        <li>After login, a dashboard page will appear.</li>
+                        <li>Add new customers and products in the respective links.</li>
+                        <li>Create bill for the customers visited.</li>
+                        <li>Click "bill" link from the bills list to see a particular bill.</li>
+                        <li>Click "Print Bill" to print/save the bill.</li> 
+                    </Typography>
                 </Grid>
 
                 <Grid item xs={6}>
